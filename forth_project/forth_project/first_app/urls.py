9412722edead8ce1f37879818649e6278c61667c
MAHRifat <hasanrifata2@gmail.com>
2023-09-12 00:45:51 +0600

    Add template inheritence

--- forth_project/forth_project/first_app/urls.py
+++ forth_project/forth_project/first_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contact/', views.home, name="home_page"),
+    path('home/', views.home, name="home_page"),
     path('about/',views.about, name="about_page"),
 ]
--- forth_project/forth_project/first_app/views.py
+++ forth_project/forth_project/first_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, './first/index.html', context= {'name': 'Phitron', 'age': 19, 'marks': 33, 'courses': [
+    return render(request, './first/home.html', context= {'name': 'Phitron', 'age': 19, 'marks': 33, 'courses': [
         {
             'id': 1,
             'course': 'C++',
