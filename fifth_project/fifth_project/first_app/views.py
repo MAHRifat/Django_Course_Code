6f129c6dd8fda0e73ead652d6a611d66b2bc529e
MAHRifat <hasanrifata2@gmail.com>
2023-09-12 14:49:26 +0600

    Form using get method

--- fifth_project/fifth_project/first_app/views.py
+++ fifth_project/fifth_project/first_app/views.py
@@ -9,8 +9,8 @@ def about(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('useremail')
-        password = request.POST.get('userpassword')
-        return render(request, './first/about.html', {'name': name,'email': email,'passwoed': password})
+        select = request.POST.get('select')
+        return render(request, './first/about.html', {'name': name,'email': email,'select': select})
     else:
         return render(request, './first/about.html')
 
