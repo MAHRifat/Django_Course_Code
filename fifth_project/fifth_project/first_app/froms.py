fea9c4c4d679cba3a602fc39625a7890b4a473c0
MAHRifat <hasanrifata2@gmail.com>
2023-09-12 20:46:04 +0600

    Add Attributes and Widget in form

--- fifth_project/fifth_project/first_app/froms.py
+++ fifth_project/fifth_project/first_app/froms.py
@@ -1,16 +1,17 @@
 from django import forms
-
+# widgets == fiend to html input
 class contactForm(forms.Form):
-    name = forms.CharField(label="Name")
-    file = forms.FileField()
-    # email = forms.EmailField(label="Email")
-    # age = forms.IntegerField(label="Age")
+    name = forms.CharField(label="Name", help_text="Total length must be within 70 characters", required=False, disabled=False, widget= forms.Textarea(attrs={'id' : 'text_area', 'placeholder' : 'Enter your name'}))
+    # file = forms.FileField()
+    # image = forms.ImageField()
+    email = forms.EmailField(label="Email")
+    age = forms.CharField(widget=forms.NumberInput)
+    BirthDay = forms.DateField(widget=forms.DateInput(attrs= {'type' : 'date'}))
     # weight = forms.FloatField()
     # balance = forms.DecimalField()
     # check = forms.BooleanField()
-    # Birth_Day = forms.DateField()
-    # appointment = forms.DateTimeField()
-    # CHOICES = [('S','Small'), ('M','Medium'), ('L','Large'), ('XL','Extra Large')]
-    # size = forms.ChoiceField(choices=CHOICES)
-    # meal = [('P','Pepperoni'), ('M','Mashroom'), ('B','Beef')]
-    # pizza = forms.MultipleChoiceField(choices=meal)
+    appointment = forms.DateTimeField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
+    CHOICES = [('S','Small'), ('M','Medium'), ('L','Large'), ('XL','Extra Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    meal = [('P','Pepperoni'), ('M','Mashroom'), ('B','Beef')]
+    pizza = forms.MultipleChoiceField(choices=meal, widget=forms.CheckboxSelectMultiple)
--- fifth_project/fifth_project/first_app/views.py
+++ fifth_project/fifth_project/first_app/views.py
@@ -36,10 +36,10 @@ def Django_Form(request):
     if request.method == 'POST':
         form = contactForm(request.POST, request.FILES)
         if form.is_valid():
-            file = form.cleaned_data['file']
-            with open('./first_app/upload/' + file.name , 'wb+') as destination:
-                for chunk in file.chunks():
-                    destination.write(chunk)
+            # file = form.cleaned_data['file']
+            # with open('./first_app/upload/' + file.name , 'wb+') as destination:
+            #     for chunk in file.chunks():
+            #         destination.write(chunk)
             print(form.cleaned_data)
             return render(request, './first/django_form.html', {'form' : form})
     else:
