400f63dcd0dcd1dc1ea438309030388e27477e50
MAHRifat <hasanrifata2@gmail.com>
2023-09-12 17:09:14 +0600

    Forms Field Add

--- fifth_project/fifth_project/first_app/froms.py
+++ fifth_project/fifth_project/first_app/froms.py
@@ -1,5 +1,15 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="user Name")
-    email = forms.EmailField(label="uesr email")
+    name = forms.CharField(label="Name")
+    email = forms.EmailField(label="Email")
+    age = forms.IntegerField(label="Age")
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    Birth_Day = forms.DateField()
+    appointment = forms.DateTimeField()
+    CHOICES = [('S','Small'), ('M','Medium'), ('L','Large'), ('XL','Extra Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    meal = [('P','Pepperoni'), ('M','Mashroom'), ('B','Beef')]
+    pizza = forms.MultipleChoiceField(choices=meal)
