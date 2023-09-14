1e71d72b502bed9f79e760c4db6c7705c7e9229e
MAHRifat <hasanrifata2@gmail.com>
2023-09-15 00:22:17 +0600

    All about Model Form

---
+++ seventh_project/seventh_project/first_app/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from first_app.models import StudentModel
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentModel
+        # fields = '__all__'
+        # fields = ['name','roll']
+        exclude =[
+            'address']
+        
+        labels = {
+            'name' : 'Student name',
+            'roll' : 'Student roll',
+            'father_name' : 'Studetn Father name',
+            'address' : 'Studetn address'
+
+        }
+        
+        widgets = {
+            'name' : forms.TextInput(attrs={'class' : 'btn btn-success text-white'}),
+            'roll' : forms.PasswordInput()
+            }
+        
+        help_texts = {
+            'name' : "Write your full name"
+        }
+        
+        error_massages = {
+            'name' : "your name is required"
+        }
