bcbf378ed132675506f1dab527b100f7d46130bd
MAHRifat <hasanrifata2@gmail.com>
2023-09-16 02:03:02 +0600

    Edit admin in Book management project

--- Book_Store_Project/book_store/book/admin.py
+++ Book_Store_Project/book_store/book/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from book.models import BookStoreModel
 # Register your models here.
 
-admin.site.register(BookStoreModel)
+# admin.site.register(BookStoreModel)
+
+class BookStoreModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book_name', 'book_author','category','first_published','last_published')
+
+admin.site.register(BookStoreModel, BookStoreModelAdmin)
--- Book_Store_Project/book_store/book/models.py
+++ Book_Store_Project/book_store/book/models.py
@@ -14,8 +14,8 @@ class BookStoreModel(models.Model):
     book_name = models.CharField(max_length=30)
     book_author = models.CharField(max_length=20)
     category = models.CharField(max_length=30, choices=CATEGORY)
-    first_published = models.DateTimeField(auto_now_add=True)
-    last_published = models.DateTimeField(auto_now=True)
+    first_published = models.DateTimeField(auto_now_add=True) # django ekdom first data
+    last_published = models.DateTimeField(auto_now=True) # erpor joto update korbo sei date dethabe
     
     def __str__(self) -> str:
         return f"{self.book_name}  - {self.book_author}"
