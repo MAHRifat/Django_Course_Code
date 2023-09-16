a0d76afbffa8d494d4aecac6524154918daef711
MAHRifat <hasanrifata2@gmail.com>
2023-09-17 01:08:29 +0600

    User logout using django buildin functions

--- Eight Project/eight_project/first_app/urls.py
+++ Eight Project/eight_project/first_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from first_app.views import signup,home,User_login,profile
+from first_app.views import signup,home,User_login,profile,user_logout
 
 urlpatterns = [
     path('', home,name="homepage"),
     path('signup/', signup, name='signup'),
     path('login/', User_login, name='login'),
     path('profile/', profile , name='profile'),
+    path('logout/', user_logout , name='logout'),
 ]
--- Eight Project/eight_project/first_app/views.py
+++ Eight Project/eight_project/first_app/views.py
@@ -9,32 +9,45 @@ def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    if request.method =="POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            messages.success(request, 'Account created successfully')
-            # messages.warning(request, 'warnign')
-            # messages.info(request, 'info')
-            # messages.error(request, 'errors')
-            form.save()
+    if not request.user.is_authenticated:
+        if request.method =="POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                messages.success(request, 'Account created successfully')
+                # messages.warning(request, 'warnign')
+                # messages.info(request, 'info')
+                # messages.error(request, 'errors')
+                form.save()
+        else:
+            form = RegisterForm()
+        return render(request, 'signup.html',{'form': form})
     else:
-        form = RegisterForm()
-    return render(request, 'signup.html',{'form': form})
+        return redirect('profile')
 
 
 def User_login(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request=request, data = request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['username']
-            userpass = form.cleaned_data['password']
-            user = authenticate(username = name, password = userpass)   #check kortechi user database e ache ki na
-            if user is not None:
-                login(request, user)
-                return redirect('profile')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data = request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['username']
+                userpass = form.cleaned_data['password']
+                user = authenticate(username = name, password = userpass)   #check kortechi user database e ache ki na
+                if user is not None:
+                    login(request, user)
+                    return redirect('profile')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'login.html', {'form' : form})
     else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form' : form})
+        return redirect('profile')
 
 def profile(request):
-    return render(request, 'profile.html',{'user' : request.user}) 
+    if request.user.is_authenticated:
+        return render(request, 'profile.html',{'user' : request.user})
+    else:
+        return redirect('login')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
