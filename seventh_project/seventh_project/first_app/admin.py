e2ae8d59657ed9a191bd4f0cf913ada01f3d481c
MAHRifat <hasanrifata2@gmail.com>
2023-09-16 14:10:25 +0600

    Model Inheritance and Abstract abse class

--- seventh_project/seventh_project/first_app/admin.py
+++ seventh_project/seventh_project/first_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from first_app.models import StudentModel
+from first_app.models import StudentModel,StudentInfoModel,TeacherInfoModel
 
 # Register your models here.
 admin.site.register(StudentModel)
+admin.site.register(StudentInfoModel)
+admin.site.register(TeacherInfoModel)
--- seventh_project/seventh_project/first_app/models.py
+++ seventh_project/seventh_project/first_app/models.py
@@ -11,3 +11,23 @@ class StudentModel(models.Model):
     def __str__(self) -> str:
         return f"{self.roll} - {self.name}"
     
+# module inheritance
+    # 1. Abstract base class
+    # 2. multitable inheritance
+    # 3. proxy model
+    
+# Abstract base class
+
+class CommonInfoClass(models.Model):
+    name = models.CharField(max_length=20)
+    city = models.CharField(max_length=10)
+    class Meta:
+        abstract = True
+
+class StudentInfoModel(CommonInfoClass):
+    roll = models.IntegerField(primary_key=True)
+    payment = models.IntegerField()
+    section = models.CharField(max_length=10)
+    
+class TeacherInfoModel(CommonInfoClass):
+    salary = models.IntegerField()
