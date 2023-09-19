fad5d9c22b24a258037aeeb7ac234e94a0b754af
MAHRifat <hasanrifata2@gmail.com>
2023-09-19 15:24:00 +0600

    Change password with and without old password

--- Eight Project/eight_project/first_app/urls.py
+++ Eight Project/eight_project/first_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from first_app.views import signup,home,User_login,profile,user_logout
+from first_app.views import signup,home,User_login,profile,user_logout,pass_change,pass_change_without_old_pass
 
 urlpatterns = [
     path('', home,name="homepage"),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', User_login, name='login'),
     path('profile/', profile , name='profile'),
     path('logout/', user_logout , name='logout'),
+    path('changepass/', pass_change , name='passchange'),
+    path('changepass_without_old_pass/', pass_change_without_old_pass , name='passchange2'),
 ]
