7b250a348bfd211ede99f7669f11550825462603
MAHRifat <hasanrifata2@gmail.com>
2023-09-23 02:02:38 +0600

    Working with the session framework in Django

--- Nine_project/ninth_project/first_app/urls.py
+++ Nine_project/ninth_project/first_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.set_session, name='home'),
     path('get_cookie/', views.get_cookie),
     path('del_cookie/', views.delete_cookie),
+    path('get_session/', views.get_session),
+    path('del_session/', views.delete_session),
 ]
--- Nine_project/ninth_project/first_app/views.py
+++ Nine_project/ninth_project/first_app/views.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 # Create your views here.
 
+# Cookie
+
 def home(request):
     response = render(request, 'home.html')
     response.set_cookie('name', 'Rifat')
@@ -19,3 +21,29 @@ def delete_cookie(request):
     response = render(request, 'delete_cookie.html')
     response.delete_cookie('name')
     return response
+
+
+# session
+
+def set_session(request):
+    data = {
+        'name' : 'Rifat',
+        'age' : 23,
+        'language' : 'Bangla'
+    }
+    print(request.session.get_session_cookie_age())     # session kotodin active thakbe second e dibe
+    print(request.session.get_expiry_date())            # session er expire day show korbe
+    request.session.update(data)
+    return render(request, 'home.html')
+
+def get_session(request):
+    name = request.session.get('name')
+    age = request.session.get('age')
+    language = request.session.get('language')
+    return render(request,'get_session.html', {'name':name, 'age': age, 'language':language})
+
+def delete_session(request):
+    # del request.session['name']           # only delete name 
+    request.session.flush()         
+    request.session.clear_expired()          # delete all session
+    return render(request, 'delete_session.html')
