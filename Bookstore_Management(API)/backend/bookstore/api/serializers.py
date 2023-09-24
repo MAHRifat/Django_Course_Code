ac57064875a516091bb16a805bb4e4bd6e45f0f1
MAHRifat <hasanrifata2@gmail.com>
2023-09-25 01:04:29 +0600

    Handling Request, Response Using ApiView

--- Bookstore_Management(API)/backend/bookstore/api/serializers.py
+++ Bookstore_Management(API)/backend/bookstore/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import BookStoreModel
 
-class SnippetSerializer(serializers.ModelSerializer):
+class BookStoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookStoreModel
         fields = '__all__'
