9f6534a2f3905c62c52c8cf2d049f94a6c35df37
MAHRifat <hasanrifata2@gmail.com>
2023-09-26 02:00:14 +0600

    How to protect api using permissions

---
+++ DRF/drf_2/first_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import  path,include
+from rest_framework import routers
+from .views import ProductViewSet, ProductReviewViewSet
+
+router = routers.DefaultRouter()
+router.register('products', ProductViewSet, basename='product')
+router.register('reviews', ProductReviewViewSet, basename='product-review')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
